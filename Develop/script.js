30c252db96779c43b69eab7af1a3d830aaf860e8
Preston Vongvilay <xprestonxd@gmail.com>
2023-05-11 19:53:08 -0700

    created repo and added JS to project

---
+++ Develop/script.js
@@ -0,0 +1,52 @@
+// Assignment code here
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var upppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = '1234567890';
+var symbols = '!@#$%^&*()_+<>:"{}|[]\;';
+var fill = '';
+// ask what they want in it 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  // math.floor.random
+  // for loop
+
+
+  passwordText.value = password;
+
+}
+function generatePassword() {
+  var length = prompt("How long would you like your password to be?");
+  var qSymbols = confirm("Would you like symbols included?");
+  var qUpper = confirm("would you like Uppercase Characters?");
+  var qLower = confirm("Would you like lowercase letters included?");
+  var qNumber = confirm("would you like numbers included?")
+  if (qSymbols === true){
+    fill += symbols
+  }
+  if (qUpper === true){
+    fill += upppercase
+  }
+  if (qLower === true) {
+    fill += lowercase
+  }
+  if (qNumber === true) {
+    fill += numbers
+  }
+
+  var keep = '';
+ // make if statements for all questions 
+  for (var i = 0; i < length; i++) {
+ var randomIndex = Math.floor(Math.random()*fill.length)
+ var randomChar = fill[randomIndex]
+ keep += randomChar
+  }
+  return keep 
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
